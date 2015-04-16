128da688062612c04ec026ebb966a8da8562fa3b
Victor Moskvych <ixeos18@gmail.com>
2015-04-16 20:46:45 +0300

    new changes

--- src/com/ixeos/Main.java
+++ src/com/ixeos/Main.java
@@ -3,6 +3,7 @@ package com.ixeos;
 public class Main {
 
     public static void main(String[] args) {
-	System.err.println("Hello");
+	    System.err.println("Hello");
+        System.err.println("World");
     }
 }
